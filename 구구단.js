4dbd7099caedfbb5916c6aa7f1af35bac6ff847d
kihnam <ki.south4@gmail.com>
2021-05-20 22:58:39 +0900

    0520

---
+++ 구구단.js
@@ -0,0 +1,15 @@
+while (true) {
+    var 숫자1 = Math.ceil(Math.random() * 9)
+    var 숫자2 = Math.ceil(Math.random() * 9)
+    var 결과 = 숫자1 * 숫자2
+    var 조건 = true;
+    while (조건) {
+        var 답 = prompt(String(숫자1) + '곱하기' + String(숫자2) + '는?')
+        if (결과 === Number(답)) {
+            alert('딩동댕');
+            조건 = false;
+        } else {
+            alert('땡');    
+        }
+    }
+}
