a2edf7dc0084bf83027dcd6d4efc55c6f4f34de8
kihnam <ki.south4@gmail.com>
2021-05-21 23:21:51 +0900

    0521

--- 끝말잇기.js
+++ 끝말잇기.js
@@ -1,24 +1,52 @@
-var word = '남기훈'
-while (true) {
-var answer = prompt(word)
-if (word[word.length - 1] === answer[0]) {
-    // 맞았을 때
-        alert('딩동댕')
-        word = answer;
-    } else {
-    // 틀렸을 때
-       alert('땡')
-    }
-}
+var 바디 = document.body;
+var 단어 = document.createElement('div');
+단어.textContent = '남기훈';
+document.body.append(단어);
+var 폼 = document.createElement('form');
+document.body.append(폼);
+var 입력창 = document.createElement('input');
+폼.append(입력창);
+var 버튼 = document.createElement('button');
+버튼.textContent = '입력';
+폼.append(버튼);
+var 결과창 = document.createElement('div');
+document.body.append(결과창);
 
-for (var word = '남기훈'; true;) {
-    var answer = prompt(word)
-if (word[word.length - 1] === answer[0]) {
-    // 맞았을 때
-        alert('딩동댕')
-        word = answer;
+폼.addEventListener('submit', function 콜백함수 (이벤트) {
+    이벤트.preventDefault();
+    if (단어.textContent[단어.textContent.length - 1] === 입력창.value[0]) {
+        결과창.textContent = '딩동댕';
+        단어.textContent = 입력창.value;
+        입력창.value = '';
+        입력창.focus();
     } else {
-    // 틀렸을 때
-       alert('땡')
+        결과창.textContent = '땡';
+        입력창.value = '';
+        입력창.focus();
     }
-}
+});
+
+// var word = '남기훈'
+// while (true) {
+// var answer = prompt(word)
+// if (word[word.length - 1] === answer[0]) {
+//     // 맞았을 때
+//         alert('딩동댕')
+//         word = answer;
+//     } else {
+//     // 틀렸을 때
+//        alert('땡')
+//     }
+// }
+
+// for (var word = '남기훈'; true;) {
+//     var answer = prompt(word)
+// if (word[word.length - 1] === answer[0]) {
+//     // 맞았을 때
+//         alert('딩동댕')
+//         word = answer;
+//     } else {
+//     // 틀렸을 때
+//        alert('땡')
+//     }
+// }
