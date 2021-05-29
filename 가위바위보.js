e676f22a6ed9ddac1bf888b1df8ab9ee268a9c80
kihnam <ki.south4@gmail.com>
2021-05-29 21:30:24 +0900

    0529

--- 가위바위보.js
+++ 가위바위보.js
@@ -1,18 +1,55 @@
-var left = 0;
-setInterval(function () {
-   if (left === 0) {
-       left = '-190px';
-   } else if (left === '-190px' ) {
-       left ='-400px';
-   } else {
-       left = 0;
-   }
-   document.querySelector('#computer').style.background = 
-   'url(./가위바위보.jpg)' + left + ' 0';
-}, 100);
+var 이미지좌표 = 0;
+var 가위바위보 = { // 가위바위보 자료구조
+    바위: '0',
+    가위: '-185px',
+    보: '-400px'
+};
+
+function 컴퓨터의선택(이미지좌표) {
+    return Object.entries(가위바위보).find(function(v) {
+        return v[1] === 이미지좌표;
+    })[0];
+}
+
+var 인터벌;
+function 인터벌메이커() {
+    인터벌 = setInterval(function () {
+    if (이미지좌표 === 가위바위보.바위) {
+        이미지좌표 = 가위바위보.가위;
+    } else if (이미지좌표 === 가위바위보.가위 ) {
+        이미지좌표 = 가위바위보.보;
+    } else {
+        이미지좌표 = 가위바위보.바위;
+    }
+    document.querySelector('#computer').style.background = 
+    'url(./가위바위보.jpg)' + 이미지좌표 + ' 0';
+    }, 100);
+}
+
+인터벌메이커();
+
+var 점수표 = {
+    가위: 1,
+    바위: 0,
+    보: -1,
+};
 
 document.querySelectorAll('.btn').forEach(function(btn) {
     btn.addEventListener('click', function() {
-        console.log(this.textContent);
+        clearInterval(인터벌);
+        setTimeout(function() {
+            인터벌메이커();
+        }, 1000);
+        var 나의선택 = this.textContent;
+        var 나의점수 = 점수표[나의선택];
+        var 컴퓨터점수 = 점수표[컴퓨터의선택(이미지좌표)];
+        var 점수차 = 나의점수 = 컴퓨터점수;
+        if (점수차 === 0) {
+            console.log('비겼습니다.');
+        } else if ([-1, 2].includes(점수차)) {
+            console.log('이겼습니다.');
+        } else {
+            console.log('졌습니다');
+        }
     });
 })
