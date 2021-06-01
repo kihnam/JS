7f4e4f94c76776c2bad48bad254b4f66b4ffcdcc
kihnam <ki.south4@gmail.com>
2021-06-02 01:51:31 +0900

    0602

--- closer.js
+++ closer.js
@@ -1,5 +1,25 @@
+// // 클로저 문제
+// for (var i = 0; i < 100; i ++) {
+//     setTimeout(function () {
+//         console.log(i); // i는 스코프에 의해 1번째 줄 i가 된다.
+//     }, i * 1000)
+// }
+
+// //클로저 해결방법
+// for (var i = 0; i < 100; i ++) {
+//     function 클로저(j) {
+//         setTimeout(function () {
+//             console.log(j);
+//         }, j * 1000)
+//     }
+//     클로저(i);
+// }
+
+//클로저 즉시실행
 for (var i = 0; i < 100; i ++) {
-    setTimeout(function () {
-        console.log(i); // i는 스코프에 의해 1번째 줄 i가 된다.
-    }, i * 1000)
+    (function 클로저(j) {
+        setTimeout(function () {
+            console.log(j);
+        }, j * 1000)
+    })(i);
 }
--- 지뢰찾기.js
+++ 지뢰찾기.js
@@ -1,8 +1,14 @@
 var tbody = document.querySelector('#table tbody');
 var dataset = [];
+var 중단플래그 = false;
+var 열은칸 = 0;
 document.querySelector('#exec').addEventListener('click', function() {
     // 내부 먼저 초기화
     tbody.innerHTML = '';
+    dataset = [];
+    열은칸 = 0;
+    중단플래그 = false;
+    document.querySelector('#result').textContent = '';
     var hor = parseInt(document.querySelector('#hor').value);
     var ver = parseInt(document.querySelector('#ver').value);
     var mine = parseInt(document.querySelector('#mine').value);
@@ -33,6 +39,9 @@ document.querySelector('#exec').addEventListener('click', function() {
             var td = document.createElement('td');
             td.addEventListener('contextmenu', function(e) {
                 e.preventDefault();
+                if (중단플래그) {
+                    return;
+                }
                 var 부모tr = e.currentTarget.parentNode;
                 var 부모tbody = e.currentTarget.parentNode.parentNode;
                 var 칸 = Array.prototype.indexOf.call(부모tr.children, e.currentTarget);
@@ -52,14 +61,22 @@ document.querySelector('#exec').addEventListener('click', function() {
                 }
             });
             td.addEventListener('click', function(e){
+                if (중단플래그) {
+                    return;
+                }
                 // 클릭했을 때 주변 지뢰 개수
                 var 부모tr = e.currentTarget.parentNode;
                 var 부모tbody = e.currentTarget.parentNode.parentNode;
                 var 칸 = Array.prototype.indexOf.call(부모tr.children, e.currentTarget);
                 var 줄 = Array.prototype.indexOf.call(부모tbody.children, 부모tr);
-                if (dataset[줄][칸] === 'X') {
+                e.currentTarget.classList.add('opened');
+                열은칸 += 1;
+                if (dataset[줄][칸] === 'X') { // 지뢰 클릭
                     e.currentTarget.textContent = '펑';
-                } else {
+                    document.querySelector('#result').textContent = '실패';
+                    중단플래그 = true;
+                } else { // 지뢰가 아닌 경우 주변 지뢰 개수
+                    dataset[줄][칸] = 1;
                     var 주변 = [
                         dataset[줄][칸-1], dataset[줄][칸+1]
                     ];
@@ -69,10 +86,48 @@ document.querySelector('#exec').addEventListener('click', function() {
                     if (dataset[줄+1]) {
                         주변 = 주변.concat([dataset[줄+1][칸-1], dataset[줄+1][칸], dataset[줄+1][칸+1]]);
                     }
-                    console.log(주변);
                     e.currentTarget.textContent = 주변.filter(function(v) {
                         return v === 'X';
                     }).length;
+                    e.currentTarget.textContent = 주변지뢰개수 || '';
+                    if (주변지뢰개수 === 0) {
+                        console.log('주변을 엽니다');
+                        var 주변칸 = [];
+                        if (tbody.children[줄-1]) {
+                            주변칸 = 주변칸.concat([
+                                tbody.children[줄 - 1].children[칸 - 1],
+                                tbody.children[줄 - 1].children[칸],
+                                tbody.children[줄 - 1].children[칸 + 1],
+                            ]);
+                        }
+                        주변칸 = 주변칸.concat([
+                            tbody.children[줄].children[칸 - 1],
+                            tbody.children[줄].children[칸 + 1],
+                        ]);
+
+                        if (tbody.children[줄+1]) {
+                            주변칸 = 주변칸.concat([
+                                tbody.children[줄 + 1].children[칸 - 1],
+                                tbody.children[줄 + 1].children[칸],
+                                tbody.children[줄 + 1].children[칸 + 1],
+                            ]);
+                        }
+                        주변칸.filter(function (v) {
+                            return !!v;
+                        }).forEach(function(옆칸) {
+                            var 부모tr = 옆칸.parentNode;
+                            var 부모tbody = 옆칸.parentNode.parentNode;
+                            var 옆칸칸 = Array.prototype.indexOf.call(부모tr.children, 옆칸);
+                            var 옆칸줄 = Array.prototype.indexOf.call(부모tbody.children, 부모tr);
+                            if (dataset[옆칸줄][옆칸칸] !== 코드표.연칸) {
+                                옆칸.click();
+                            }
+                        });
+                   }
+                }
+                if (열은칸 === hor * ver - mine) {
+                    중단플래그 = true;
+                    document.querySelector('#result').textContent = '승리';
                 }
             });
             tr.appendChild(td);
